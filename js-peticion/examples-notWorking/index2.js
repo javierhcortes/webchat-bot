2fc0fe3fb97e6d91d4fd9e0dbeb19a10433f8cef
Javier-dpto-linux <jherrera@sicom.cl>
2020-06-29 17:54:02 -0400

    inicio ejemplos funcionales para conectarse a la api rest

--- js-peticion/examples-notWorking/index2.js
+++ js-peticion/examples-notWorking/index2.js
@@ -1,18 +1,19 @@
-// Error de cors
 var myHeaders = new Headers();
 myHeaders.append("Content-type", "application/json");
-myHeaders.append("Access-Control-Allow-Origin", "*");
+myHeaders.append("X-Auth-Token", "");
+myHeaders.append("X-User-Id", "");
 
-var raw = JSON.stringify({"user":"myusername","password":"mypassword"});
+var raw = JSON.stringify({"user":"javier","password":"admin123"});
 
 var requestOptions = {
   method: 'POST',
   headers: myHeaders,
   body: raw,
-  redirect: 'follow'
+  redirect: 'follow',
+  mode: 'no-cors'
 };
 
 fetch("http://127.0.0.1:3000/api/v1/login", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
-  .catch(error => console.log('error', error)); 
+  .catch(error => console.log('error', error));
--- js-peticion/readme.md
+++ js-peticion/readme.md
@@ -1,2 +1,8 @@
  
-#estos codigos son solo ejemplos, ninguna ha funcioando completamente
+# Codigos ejemplo de conexion a la REST API de rocket CHAT
+
+- Carpeta Examples notWorking fueron las primeras iteraciones
+- Carpeta AxiosExamples son los ejemplos funcionales con axios, para ejecutarla
+por cmd ejecutar
+        
+        node file.js
